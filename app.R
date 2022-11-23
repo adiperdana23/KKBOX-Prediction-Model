2d29ae92285a19c53516c1616906f59160e57524
Adhi Perdana Putra <100086394+adiperdana23@users.noreply.github.com>
2022-11-23 11:34:34 +0700

    Updated shinny dashboard, remove unnecessary libary
    unnecessary library is removed

--- app.R
+++ app.R
@@ -7,25 +7,15 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+#library(shiny)
 library(plotly)
 library(glue)
-library(psych)
-library(forcats)
-library(stargazer)
 library(caret)
 library(gmodels)
-library(BBmisc)
-library(partykit)
 library(lubridate)
-library(psych)
-library(rpart)
-library(rattle)
-library(rpart.plot)
 library(caret)
 library(GGally)
 library(yardstick)
-library(randomForest)
 library(ggplot2)
 library(hrbrthemes)
 library(dplyr)
@@ -38,28 +28,20 @@ library(shinythemes)
 # Model Dataset
 ## Load the dataset for model
 
-# df train
-wsdm_finaldf_train <- readRDS("wsdm_finaldf_train.RDS")
-head(wsdm_finaldf_train)
-# df test for decision tree
-wsdm_finaldf_test <- readRDS("wsdm_finaldf_test.RDS")
-# tuning model for logis 
-wsdm_model2_test <- readRDS("wsdm_model2_test.RDS")
-wsdm_model2_train <- readRDS("wsdm_model2_train.RDS")
 
 
 # For Listening hour
-forest_viz_ggplot <- readRDS("forest_viz_ggplot.RDS")
+forest_dataviz_ggplotly <- readRDS("forest_dataviz_ggplotly .RDS")
 #For loyalty and  churn
 forest_loyalty_scattter_tooltip.RDS <- readRDS("forest_loyalty_scattter_tooltip.RDS")
 # For Registration line plot
-wsdm_for_registration <- readRDS("wsdm_for_registration.RDS")
+wsdm_for_registration_plot<- readRDS("wsdm_for_registration_plot.RDS")
 # For auto renew
 auto_renew_plot <- readRDS("auto_renew_plot.RDS")
 
 
-# Variable Importance Random Forest
-data_viz_varmp <-readRDS("data_viz_varmp.RDS")
+# Variable Importance Random Forest 
+varimp_forest_plotly <-readRDS("varimp_forest_plotly.RDS")
 
 #theme
 
@@ -220,19 +202,7 @@ server <- function(input, output) {
     },deleteFile =F)
     
     
-    output$listen_plot <- renderPlotly({ggplot(forest_viz_ggplot, aes(fill=condition, y=listening_hour, x=value,text=paste0( "Churn or stay: ", condition, "<br>",
-                                                                                                                             "number of subscriber: ", scales::comma(value, 1), "<br>"))) + 
-        geom_bar(position="stack", stat="identity") +
-        scale_x_continuous(labels = scales::comma) +
-        scale_fill_viridis(discrete = T) +
-        ggtitle("churn by listening hour") +
-        labs(x = 'Number of Subscriber',
-             y = ' Listening Hours',
-             title = 'Number of churned customer by listening hour')+
-        theme_algoritma
-      
-      ggplotly(forest_dataviz_ggplot, tooltip = "text")
-      
+    output$listen_plot <- renderPlotly({forest_dataviz_ggplotly 
       
       
     })
@@ -246,34 +216,14 @@ server <- function(input, output) {
       
       
     })
-  output$growth_plot <- renderPlot({wsdm_for_registration  %>%
-      head(14) %>%
-      ggplot( aes(x=year_registration, y=`number of subscriber`)) +
-      geom_line() +
-      geom_point(color="#69b3a2", size=4) +
-      scale_y_continuous(labels = scales::comma)  +
-      ggtitle("Subscriber Growth year to year") +
-      ylab("number of subscriber") +
-      theme_ipsum()})
+  output$growth_plot <- renderPlot({wsdm_for_registration_plot
+      })
   
   output$renew_plot <-renderPlotly({auto_renew_plot
     
     ggplotly(auto_renew_plot, tooltip = "text")})
   
-  output$varimp_forest <-renderPlotly({ggplot(data_viz_varmp, aes(x = `mean decrease gini`, y = reorder(variable, `mean decrease gini`))) +
-      geom_col(aes(fill = `mean decrease gini`, text = tooltip)) +
-      scale_fill_continuous(low = "gray", high = "blue") +
-      guides(fill = "none") +
-      labs(
-        title = "Variable importance in Random Forest Model",
-        subtitle = "KKBOXX Online Music Streaming",
-        x = "variable importance",
-        y = NULL
-      ) +
-      theme_minimal()
-    ggplotly(varimp_forest_plotly, tooltip = "text")
-    
-    
+  output$varimp_forest <-renderPlotly({varimp_forest_plotly
     
     
     
